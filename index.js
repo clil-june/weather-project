e90d55402d4ef963adce6f49bc5a570cbcddf026
Clil SneOR <clil.s.au@gmail.com>
2021-02-24 12:29:52 +0200

    activated location button and enabled finding weather from local location

--- index.js
+++ index.js
@@ -49,6 +49,49 @@ function showWeather(response){
   iconElemant.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
   
 }
+
+function searchLocation (position){
+  let apiKey = "e75376106dace0797a632d47f62a8825";
+  let lat = position.coords.latitude;
+  let lon= position.coords.longitude;
+  let locationUrl =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`;
+  axios.get(`${locationUrl}&appid${apiKey}`).then(showLocalWeather);
+}
+
+function getLocation (){
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function showLocalWeather (response){
+  let cityName = document.querySelector("#location");
+  let locationName = (response.data.name);
+  cityName.innerHTML=(locationName);
+
+  let temperature = document.querySelector("#temperature");
+  temp=(response.data.main.temp);
+  temperature.innerHTML=Math.round(`${temp}`)
+
+  let discription=document.querySelector("#dicription");
+  let disc = response.data.weather[0].description;
+  discription.innerHTML=(`${disc}`);
+
+  let wind=document.querySelector("#wind");
+  let windSpeed= Math.round(response.data.wind.speed);
+  wind.innerHTML=(`<i class="fas fa-wind"></i> Wind speed: ${windSpeed} km/h`)
+
+  let hume=document.querySelector("#humidity");
+  let humidity=Math.round(response.data.main.humidity);
+  hume.innerHTML=(`<i class="fas fa-tint"></i> Humidity: ${humidity}%`)
+
+  let iconElemant = document.querySelector("#icon");
+  let icon= response.data.weather[0].icon;
+  iconElemant.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+  
+}
+
+let locationButton = document.querySelector("#your-location")
+locationButton.addEventListener("click",getLocation);
+
 function displayFahr (event){
   event.preventDefault();
   let temperature = document.querySelector("#temperature");
